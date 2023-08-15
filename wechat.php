d0e4050ef9b5e940153d07d55af0aaed69a576b6
Jmz <70007214+smujm@users.noreply.github.com>
2023-08-15 10:49:25 +0800

    Update wechat.php

--- wechat.php
+++ wechat.php
@@ -22,7 +22,7 @@ $weekarray=array("日","一","二","三","四","五","六");
     $data = [
         'touser' => '',
         'template_id' => $start->getTemplateList()['template_list'][0]['template_id'],//默认只给第一个模板发消息
-        'url' => 'https://ozxc.cn',
+        'url' => 'https://github.com/smujm',
         'topcolor' => '#fdb3b0',
         'data' => [
             'date' => [
@@ -68,19 +68,19 @@ $weekarray=array("日","一","二","三","四","五","六");
             'togetherdays' => [//在一起多久了
                 'value' => $start->getTogetherDays(),
                 'color' => '#8218e7'
-            ],
-            'currentConfirm' => [//现有确诊
-                'value' => $start->getFeiYan()['现有确诊'],
-                'color' => '#ff6330'
-            ],
-            'lastLocalSureNew' => [//新增确诊
-                'value' => $start->getFeiYan()['新增确诊'],
-                'color' => '#ff6330'
-            ],
-            'lastIncrHideNew' => [//新增无症状
-                'value' => $start->getFeiYan()['新增无症状'],
-                'color' => '#e62b3b'
             ]
+            // 'currentConfirm' => [//现有确诊
+            //     'value' => $start->getFeiYan()['现有确诊'],
+            //     'color' => '#ff6330'
+            // ],
+            // 'lastLocalSureNew' => [//新增确诊
+            //     'value' => $start->getFeiYan()['新增确诊'],
+            //     'color' => '#ff6330'
+            // ],
+            // 'lastIncrHideNew' => [//新增无症状
+            //     'value' => $start->getFeiYan()['新增无症状'],
+            //     'color' => '#e62b3b'
+            // ]
         ],
     ];
 
